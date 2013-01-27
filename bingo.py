e905f57d448c0f289c7a68cb5793b841713e4ab5
Matthew Leingang <leingang@nyu.edu>
2013-01-27 16:23:53 -0500

    modernize script, update README
    * Added a docstring in python custom instead of RDF
    * Implemented options with argparse
    * Added print-to-stdout option (default)
    * updated README file

--- bingo.py
+++ bingo.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python
-"""
-  <rdf:Description about="bingo.py"
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-    xmlns:dc="http://purl.org/dc/elements/1.1/"
-    xmlns:foaf="http://xmlns.com/foaf/1.0"
-    xmlns:dct="http://purl.org/dc/terms/"> 
-    <dc:description>
-      Generate a bingo card for Icebreaking
-    </dc:description>
-    <dc:creator>
-      <foaf:Person>
-	<foaf:name>Derek Bruff</foaf:name>
-      </foaf:Person>
-    </dc:creator>
-    <dct:created>2005-02-04</dct:created>
-    <dc:creator>
-      <foaf:Person>
-	<foaf:name>Matthew Leingang</foaf:name>
-	<foaf:mbox_sha1sum>9a4b7887cf33bd8142613c0832ba2710d242999c</foaf:mbox_sha1sum>
-      </foaf:Person>
-    </dc:creator>
-    <dct:created>2006-02-05</dct:created>
-    <dct:modified>2007-06-23</dct:modified>
-  </rdf:Description>
+"""Generate a bingo card for Icebreaking
+
+Authors
+=======
+
+Derek Bruff and Matthew Leingang
+
+Date
+====
+Created 2006-02-05.  Modified once or twice a year since then.
+
+Usage
+=====
+
+Read event.xml file as argument, writes a bingo card to stdout.  If you want more than 
+one, use the -n <number> option, and <number> files will be written.  They will have
+file names of the form `card%02d.html` unless the -f/--format option is specified.
 """
 
-import sys, getopt, re
+import sys, getopt, re, logging, argparse
 import random
 
 from xml.sax import ContentHandler, parse, ErrorHandler
@@ -83,15 +76,19 @@ class BingoCard:
 
 
 class BingoCardWriter:
-    """Class to write Card to File"""
-    
-    def __init__(self,fileName):
-	self.fileName=fileName
-	  
-    def write(self,card):
-	"""Write card to file"""
-	self.output=open(self.fileName,'w')
-	self.output.writelines("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+	"""Class to write Card to File"""
+
+	def __init__(self):
+		pass
+
+	def writeToFile(self,card,fileName):
+		"""Write card to file"""
+		self.output=open(fileName,'w')
+		self.output.writelines(self.toString(card))
+	
+	def toString(self,card):	
+		"""serialize card as string"""
+		output="""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
   <head>
     <title>Name Bingo</title>
@@ -126,53 +123,55 @@ class BingoCardWriter:
 
   <table border="2" cellspacing="0" cellpadding="4" align="center" id="bingoCard">
   
-""")
-	# top row is categories
-	categories = card.categories
-	self.output.writelines("    <tr>\n")
-	for category in categories:
-	    self.output.writelines("      <th>%s</th>\n" % category)
-	self.output.writelines("    </tr>\n")
-	for row in card.cells:
-	    self.output.writelines('    <tr>\n')
-	    for cell in row:
-		self.output.writelines('      <td width="120" height="120" align="center" valign="center">%s</td>\n' % cell)	
-	    self.output.writelines("    </tr>\n")
-	self.output.writelines("""  </table>
+"""
+		# top row is categories
+		categories = card.categories
+		output+="    <tr>\n"
+		for category in categories:
+		    output+="      <th>%s</th>\n" % category
+		output+="    </tr>\n"
+		for row in card.cells:
+			output+='    <tr>\n'
+			for cell in row:
+				output+='      <td width="120" height="120" align="center" valign="center">%s</td>\n' % cell
+			output+="    </tr>\n"
+		output+="""  </table>
 </body>
 </html>
-""")
-
-	self.output.close()
-	print >> sys.stderr, "Done!\n"
-
+"""
+		return output
+	
 def usage():
     print main.__doc__
 
 def main():
-    """Usage bingo.py [options]"""
-
-    try:
-	opts, args = getopt.getopt(sys.argv[1:], "hn:", ["help", "number="])
-    except getopt.GetoptError:
-        # print help information and exit:
-        usage()
-        sys.exit(2)
-    n=1
-    for o,a in opts:
-	if o in ("-h","--help"):
-	    usage()
-	    sys.exit()
-	if o in ("-n","--number"):
-	    n=int(a)
-    handler=EventsParser(sys.stdout)
-    parse("events.xml",handler,handler)
- 
-    for i in range(1,n+1):
-	outputFileName="card%02d.html" % i
-	card=BingoCard(handler.events)
-	writer=BingoCardWriter(outputFileName)
-	writer.write(card)
+	"""Usage bingo.py [options]"""
+	argparser = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.RawTextHelpFormatter)
+	argparser.add_argument('--verbose',help='be verbose',
+                    action='store_const',const=logging.INFO,dest='debug_level',
+                    default=logging.WARNING)
+	argparser.add_argument('--debug',help='show debugging statements',
+                    action='store_const',const=logging.DEBUG,dest='debug_level',
+                    default=logging.WARNING)
+	argparser.add_argument('-n','--number',metavar='NUMBER',help='generate and save NUMBER cards',
+					action='store',dest='n',type=int)
+	argparser.add_argument('eventfile',help='events XML file',
+					action='store',
+					default='events.xml')
+	args = argparser.parse_args()
+	logging.basicConfig(level=args.debug_level)	
+	handler=EventsParser(sys.stdout)
+	parse(args.eventfile,handler,handler)
+	cardwriter=BingoCardWriter()
+	if args.n:
+		for i in range(1,args.n+1):
+			outputFileName="card%02d.html" % i
+			card=BingoCard(handler.events)
+			cardwriter.writeToFile(card,outputFileName)
+	else:
+		# just do one and print to stdout		
+		card=BingoCard(handler.events)
+		print cardwriter.toString(card)		
 	
 if __name__== "__main__":
     main()
